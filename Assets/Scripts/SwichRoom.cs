f47dceadf8902b856d61153da082f93ea6c6751d
AdrianVides56 <adrianvj18@gmail.com>
2021-12-23 00:18:47 -0500

    Added functionality to hotspots
    - When a hotspot is pressed the player will be moved to the corresponding room
    - Added fade transition to avoid disorientation and discomfort

---
+++ Assets/Scripts/SwichRoom.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class SwichRoom : MonoBehaviour
+{
+    public Animator animator;
+
+    public void NextRoom(GameObject nextRoom)
+    {
+        animator.Play("FadeOut");
+        StartCoroutine(WaitToLoad(nextRoom));
+    }
+
+    private IEnumerator WaitToLoad(GameObject nextRoom)
+    {
+        yield return new WaitForSeconds(1);
+        nextRoom.SetActive(true);
+        animator.Play("FadeIn");
+        transform.parent.gameObject.SetActive(false);
+    }
+}
